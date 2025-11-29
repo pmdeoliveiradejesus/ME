9da0d2c0af1847a6344a32dc2731186e5ed8cd19
Paulo M. De Oliveira-De Jesus <37005800+pmdeoliveiradejesus@users.noreply.github.com>
2025-11-29 13:59:47 -0500

    Delete M3_Taller_de_Micro/taller 2 directory

--- M3_Taller_de_Micro/taller 2/tools/restr.m
+++
@@ -1,8 +0,0 @@
-function [C,Ceq]= myfun(X);
-global mo pmin pgmin pgmax md pmax n
-% min -SW = SC = sum_1^ (pmin*Pg+ 0.5*mo*Pg^2)-(pmax*Pd-0.5*md*Pd^2) 
-% st
-% rho=pmax-md*Pd
-% sum Pg = Pd
-Ceq=sum(X)-2*X(n+1);
-C=[];
